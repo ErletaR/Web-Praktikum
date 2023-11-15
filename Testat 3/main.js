e27380beccd8b548e5815341fdeac9dff45527c0
ErletaR <err0390@thi.de>
2023-11-15 15:51:30 +0100

    main.js file added + js einbinden

---
+++ Testat 3/main.js
@@ -0,0 +1,5 @@
+window.backendURL = "https://online-lectures-cs.thi.de/chat/84da5540-a712-4b67-a700-3bbd5633100d";
+//token für tom
+window.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjk5OTc3NTM1fQ.q7Dhc-cVLgpgfc9RlyrZNXQE8skc9r4xIOFGwjHhNPY";
+//token für jerry
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE2OTk5Nzc1MzV9.6dOl6Pa7aHtAjvCVwBcD1q1wNOHXKSgKk5lTiierjzs
