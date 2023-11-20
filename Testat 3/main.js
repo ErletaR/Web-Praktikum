e61202db16d3c74df9b8b67349524e5699f0e9c3
Jessica Trojna <jeffsemail@web.de>
2023-11-20 10:26:15 +0100

    dfghjkl

--- Testat 3/main.js
+++ Testat 3/main.js
@@ -2,11 +2,11 @@
 window.backendURL = "https://online-lectures-cs.thi.de/chat/4f9b8bf6-2349-44b0-9854-8bab2c105da9";
 //token für tom
 //window.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNjk5OTc3NTM1fQ.q7Dhc-cVLgpgfc9RlyrZNXQE8skc9r4xIOFGwjHhNPY";
-//window.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNzAwMTIxODI0fQ.Q1zk9O-rIDPts-QhbNpN8ukWajWtUdRBwLt_Jw9MQdI"
+window.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNzAwMTIxODI0fQ.Q1zk9O-rIDPts-QhbNpN8ukWajWtUdRBwLt_Jw9MQdI"
 //token für jerry
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE2OTk5Nzc1MzV9.6dOl6Pa7aHtAjvCVwBcD1q1wNOHXKSgKk5lTiierjzs
 
-window.token =localStorage.token;
+//window.token =localStorage.token;
 var users = [];
 var user = "Tom";
 var friends = [];
@@ -249,7 +249,7 @@ function checkRegisterInput() {
     return returnVal;
 }
 
-//friends
+//chat
 
 function getChatpartner() {
     const url = new URL(window.location.href);
@@ -287,8 +287,9 @@ function getChatpartner() {
             div.className="container";
             div.id= "chatnachricht";
             div3.className= "timestamp";
-            div2.innerHTML= messages[i].msg;
-            div3.innerHTML = messages[i].time;
+            div2.innerHTML=  messages[i].from + ": " + messages[i].msg;
+            var time = new Date(messages[i].time)
+            div3.innerText = time.getHours() + ":" + time.getMinutes()  + ":" + time.getSeconds();
             document.getElementById("chat").appendChild(div);
             div.appendChild(div2);
             div.appendChild(div3);
@@ -317,12 +318,11 @@ function getChatpartner() {
 
     //login
     function initLogin() {
-        document.getElementById('submit').addEventListener('click', function() {
+       
             const username = document.getElementById('usernameInput').value;
             const password = document.getElementById('passwordInput').value;
             user = username;
             login(username, password);
-        });
     }
     
     function login(uname, pwd) {
